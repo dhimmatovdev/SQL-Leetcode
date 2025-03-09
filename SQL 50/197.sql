aec8a5347edf8cae88ede6e1a8511188662bb849
Dilshod Himmatov <himmatovdilshod06@gmail.com>
2025-03-09 08:21:25 +0500

    09:03

--- SQL 50/197.sql
+++ SQL 50/197.sql
@@ -4,4 +4,14 @@
 --     select temperature from Weather where id = ws1.id - 1)
 -- with ws as (select * from Weather order by 2 asc)
 
-select ws.id from Weather as ws where ws.temperature > (select temperature from Weather where recordDate = ws.recordDate - 1)
+-- select ws.id from Weather as ws where ws.temperature > (select temperature from Weather where recordDate = ws.recordDate - 1)
+
+
+select current_day.id
+from Weather as current_day
+where exists (
+    select 1
+    from Weather as yesterday
+    where current_day.temperature > yesterday.temperature
+        and current_day.recordDate = yesterday.recordDate + 1 
+);
